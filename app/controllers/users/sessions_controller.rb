1cce548073c8aed0c55655edc8c7724d653099ef
augustoospina1610 <caor1993.b787@gmail.com>
2016-08-03 16:05:03 -0500

    novedad

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
+
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -7,9 +8,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    #custon sign-in code #super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -23,4 +24,9 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   devise_for :users, controller: {sessions: 'users/sessions'}
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_scope :user do |inicio|
+    get "sign_in", to: "devise/sessions#new"
+  end
 end
+
